de2143343eb220b2112262670a7e8fad993b88ba
itxinho <italopithu@hotmail.com>
2013-11-11 15:20:39 -0200

    #1 UPDATE instalador

--- install/instaler.php
+++ install/instaler.php
@@ -1,10 +1,5 @@
 <?php if(!defined('BASEPATH')) exit('Falha no carregamento do script!');
 
- $host = NULL;
-$port = NULL;
-$user = NULL;
-$senha = NULL; 
-$dbname = NULL;
 if(isset($_REQUEST['install'])){
 if(ini_get('allow_url_fopen') == 1){
       
@@ -22,27 +17,29 @@ if(ini_get('allow_url_fopen') == 1){
  */
   class configDB{
 
-            protected \$db_host  = '{$_REQUEST['host']}';
+            protected \$db_host  = '{$_POST['host']}';
             protected \$db_driver = 'mysql';
-            protected \$db_port ='{$_REQUEST['port']}';
-            protected \$db_user = '{$_REQUEST['user']}';
-            protected \$db_senha = '{$_REQUEST['senha']}';
-            protected \$db_name = '{$_REQUEST['dbname']}';
+            protected \$db_port ='{$_POST['port']}';
+            protected \$db_user = '{$_POST['user']}';
+            protected \$db_senha = '{$_POST['senha']}';
+            protected \$db_name = '{$_POST['dbname']}';
    }");
 
         fclose($fp);
         
             print 'criado com sucesso!';
             redirecionar('action=index');
-                 }
-            else {
-          print 'Falha ao tentar criar arquivo!';    
          }
+          else {
+              print 'Falha ao tentar criar arquivo!';    
+        }
  
     }
-    else {
+   else {
            print 'fopen desativo!';
         }
+    
+    
  }
  ?>
 <!DOCTYPE html>
@@ -80,11 +77,11 @@ if(ini_get('allow_url_fopen') == 1){
             <legend>Informações da base de dados</legend>
             <form action="" method="POST" id="dadosDB">
                 <label>Host:</label><br><input type="text" name="host" /><br>
-                <label>Porta:</label><br><input type="text" name="port" /><br>
+                <label>Porta:</label><br><input type="number" name="port" /><br>
                 <label>Usuario:</label><br><input type="text" name="user" /><br>
                 <label>Senha:</label><br><input type="text" name="senha" /><br>
                 <label>DbName:</label><br><input type="text" name="dbname" /><br>
-                <input type="submit" class=""   name="install" value="Próximo" />
+                <input type="submit"  name="install" value="Próximo" />
             </form>
             </fieldset>
         </div>
