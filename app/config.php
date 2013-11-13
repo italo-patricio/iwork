effda279152f214bb084c8f764c95a4eeb9a0e81
Augusto Lima <augustoolima@hotmail.com>
2013-11-13 13:50:50 -0300

    App config

---
+++ app/config.php
@@ -0,0 +1,17 @@
+<?php
+
+// Configuração da aplicação
+
+return array(
+        'name'=>'Nome do projeto',
+        'defaultController'=>'site',
+        'defaultAction'=>'index',
+        'mysql' => array(
+			'address'  => 'localhost',
+			'driver'   => 'mysql',
+			'port'     => 1212,
+			'username' => 'root',
+			'password' => '!@#',
+			'dbname'   => 'nomedobanco'
+        )
+);
