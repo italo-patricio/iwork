7db4b8022f75f2bfd22a3d79ce38a21a05b50551
Augusto Lima <augustoolima@hotmail.com>
2013-11-16 01:20:57 -0300

    application files

---
+++ application/views/error/code500.php
@@ -0,0 +1,1 @@
+<center style="margin-top:100px"><h1>Error 500</h1></center>
---
+++ application/views/site/index.php
@@ -0,0 +1,10 @@
+<!--
+	Início de conteúdo
+-->
+
+<h4>SiteController <b>/</b> indexAction</h4>
+$_SESSION['teste'] = <?php echo $_SESSION['teste']; ?> => OK
+
+<!--
+	Fim de conteúdo
+-->
